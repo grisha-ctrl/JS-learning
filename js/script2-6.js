6a88e5b1239bd56f6add05feae6c261237a31c61
grisha_ubica_rus76 <grisha2002grisha@mail.ru>
2020-07-27 20:12:51 +0300

    first commit

---
+++ js/script2-6.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let number = 4.5;
+
+console.log(4/0);
+console.log(-4/0);
+console.log('string' * 9); //(Not a Number)
+
+const persone = 'Alex';
+
+const bool = true;
+
+console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+console.log(obj['name']);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
---
+++ js/script2-8.js
@@ -0,0 +1,10 @@
+const category = 'toys'; // инторполяция (возможность вставлять прямо в струку переменную)
+
+console.log('https://someurl.com/' + category + '/' + '4');
+
+console.log(`https://someurl.com/${category}/5`); // получается тоже что и сверху (упрощённая версия)
+
+const user = "Grisha";
+
+alert(`Привет, ${user}`);
+
