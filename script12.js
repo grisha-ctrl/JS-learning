0cc098787b0cb27313c238bbbf9220fe493ce324
grisha_ubica_rus76 <grisha2002grisha@mail.ru>
2020-07-31 21:31:50 +0300

    начало задач

--- script12.js
+++ script12.js
@@ -13,7 +13,7 @@ const a = prompt("Один из последних просмотренных ф
       b = prompt("На сколько оцените его?", ""),
       c = prompt("Один из последних просмотренных фильмов?", ""),
       d = prompt("На сколько оцените его?", "");
-      
+
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
