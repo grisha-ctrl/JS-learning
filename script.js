2c886d2efac0b05582dc9e493c91ea08f8fc5ff9
grisha_ubica_rus76 <grisha2002grisha@mail.ru>
2020-07-31 20:13:52 +0300

    git

---
+++ script.js
@@ -0,0 +1,5 @@
+// git log просмотр сохранений
+// git add <file>... to update what will be commited
+// git checkout -- <file>... to discard changes in working directory
+// git add -A добавить все файлы
+// git commit -a -m"..." сохранение + пометка что изменилось    
